8ecd4ec6d8809a64b7584809cd7f61cee85b40ec
billie <bxllxebxngx@gmail.com>
2019-10-04 15:40:51 +0800

    dummy1

---
+++ backend/insert_data.py
@@ -0,0 +1,138 @@
+
+di = [
+    {
+        "lat": 1.284566,
+        "lon": 103.843626,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284556,
+        "lon": 103.843696,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.318214,
+        "lon": 103.893133,
+        "town": "Paya Lebar",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.306691,
+        "lon": 103.849396,
+        "town": "Little India",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284566,
+        "lon": 103.843626,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284766,
+        "lon": 103.843526,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284866,
+        "lon": 103.843826,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.304041,
+        "lon": 103.831792,
+        "town": "Orchard",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284566,
+        "lon": 103.842826,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.284566,
+        "lon": 103.843826,
+        "town": "chinatown",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    },
+    {
+        "lat": 1.311532,
+        "lon": 103.871372,
+        "town": "Kallang",
+        "datetime": "2019-10-02 08:30:00",
+        "gender": "M",
+        "ethnicity": "chinese",
+        "ageGroup": 40,
+        "additionalRemarks": "wear white",
+        "risk": "10",
+        "homelessCount": 1
+    }
+]
+
+def get_d():
+    return di
