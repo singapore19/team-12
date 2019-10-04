1f4177524bf20682e21db6c5aa431fc8164af393
harrychengly <harrychengly@gmail.com>
2019-10-04 16:04:29 +0800

    added pics

--- my-app/src/components/homePage.js
+++ my-app/src/components/homePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Col, Row , Button, Radio, Form, Icon, Input, Upload, Rate, Switch, message} from 'antd';
+import { Card, Col, Row , Button, PageHeader} from 'antd';
 import ApiWrapper from './Map/MapLocation.js'
 import { Link } from "react-router-dom";
 
@@ -11,6 +11,10 @@ class homePage extends Component {
       return(
         <div style={{ marginTop: 16, marginBottom:16 ,width:"100%", height:"50vh", }}>
         <div type="flex" justify="space-around" align="middle">
+        <PageHeader title="New Hope Community Services" />
+        <img height="200" width="200" src={require('./nhwc.jpg')} />
+
+     <div style={{ marginTop: 16 }}>
     <Row gutter={16}>
       <Col span={12}>
        <Button type="primary" block size="large">
@@ -26,6 +30,7 @@ class homePage extends Component {
         </Link>
       </Col>
     </Row>
+    </div>
     </div>
     </div>
       )
